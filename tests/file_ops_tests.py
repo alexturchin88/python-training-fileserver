b0735089fdd8b3bc4e2b68df0124167c93cf9f69
ATurchin <alexey.turchin@dxc.com>
2023-02-27 17:51:58 +0200

    Added pytests template

---
+++ tests/file_ops_tests.py
@@ -0,0 +1,18 @@
+import pytest
+from file_operations import file_ops
+
+
+def test_create_file():
+    pass
+
+
+def test_delete_file():
+    pass
+
+
+def test_get_file_metadata():
+    pass
+
+
+def test_read_file():
+    pass
